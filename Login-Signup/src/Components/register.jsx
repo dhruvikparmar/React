268389a7024399de18ade5e166b1d2da4806ebd9
dhruvikparmar <dhruvikparmar1008@gmail.com>
2025-07-07 10:36:10 +0530

    login-Signup

--- Login-Signup/src/Components/register.jsx
+++ Login-Signup/src/Components/register.jsx
@@ -28,14 +28,20 @@ export default function register() {
         const updatedRecord = [...record, obj];
         setRecord(updatedRecord);
         localStorage.setItem("userData", JSON.stringify(updatedRecord));
-        if(formdata.name || formdata.number || formdata.email || formdata.password == "")
+        if(formdata.name || formdata.number || formdata.email || formdata.password )
         {
-            swal.fire({
+            Swal.fire({
                 title: 'Registration Successful',
                 text: 'You can now login',
                 icon: "success"
             });
         }
+        setFormdata({
+            name:"",
+            number:"",
+            email:"",
+            password:""
+        });
     };
 
 
@@ -44,10 +50,10 @@ export default function register() {
         <center>
             <h1 style={{fontSize:'50px',}}>User Registration</h1>
             <div style={{marginTop:'60px',lineHeight:'2'}}>
-                <input type='text'  placeholder='Enter your name' name='name' onChange={handleChange}/><br/><br/>
-                <input type='number' placeholder='Enter your age' name='number' onChange={handleChange}/><br/><br/>
-                <input type='email' placeholder='Enter email' name='email' onChange={handleChange}/><br/><br/>
-                <input type='password' placeholder='Enter password' name='password' onChange={handleChange}/><br/><br/>
+                <input type='text'  placeholder='Enter your name' name='name' value={formdata.name} onChange={handleChange}/><br/><br/>
+                <input type='number' placeholder='Enter your age' name='number'  value={formdata.number} onChange={handleChange}/><br/><br/>
+                <input type='email' placeholder='Enter email' name='email' value={formdata.email}  onChange={handleChange}/><br/><br/>
+                <input type='password' placeholder='Enter password' name='password' value={formdata.password} onChange={handleChange}/><br/><br/>
             </div>
             <button style={{marginTop:'20px'}} onClick={handleregister}>Register</button>
             <button style={{marginLeft:'20px'}} onClick={()=>navigate("/login")}>Login</button>
